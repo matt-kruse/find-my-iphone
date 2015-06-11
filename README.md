eafe7d59a2774107d6f808834c7927dd33fd3883
Matt Kruse <github@mattkruse.com>
2015-06-11 07:28:53 -0500

    Made label optional, added callback

--- README.md
+++ README.md
@@ -18,17 +18,28 @@ It works by pretending to be a browser and actually logging into the iCloud serv
 
 ```javascript
 var find = require('find-my-iphone');
+
+// Alert the first (or only) device on the account
+find('user@icloud.com', 'password');
+
+// Alert a specific device
 find('user@icloud.com', 'password', 'iPhone 6');
+
+// Callback when successful
+find('user@icloud.com', 'password', 'iPhone 6',function() {
+	console.log("Done!");
+});
 ```
 
 # API
 
 ```javascript
-find(email,password,label)
+find(email,password[,label[,callback]])
 ```
 
- * email : The email address you use to login to iCloud
- * password: Your iCloud password
- * label : The label of the phone you want to alert (iCloud accounts may have multiple devices). The label can be found under "All Devices" in the Find My iPhone app. If there are multiple phones with the same label, the last one matching will be alerted. You should rename devices to be unique.
+ * email (required): The email address you use to login to iCloud
+ * password (required): Your iCloud password
+ * label (optional): The label of the phone you want to alert (iCloud accounts may have multiple devices). The label can be found under "All Devices" in the Find My iPhone app. If there are multiple phones with the same label, the last one matching will be alerted. You should rename devices to be unique.
+ * callback (optional): A function to execute when the phone has been alerted
 
  
--- index.js
+++ index.js
@@ -6,31 +6,36 @@ var handle_error = function(res) {
 		throw res.statusMessage;
 	}
 }
-var find_my_iphone = function(apple_id,password,device_name) {
+var find_my_iphone = function(apple_id,password,device_name,callback) {
 	var iRequest = request.defaults({jar: true,headers:{"Origin":"https://www.icloud.com"}});
 	iRequest.post({"url":'https://setup.icloud.com/setup/ws/1/login', json: {'apple_id':apple_id,'password':password}},
 		function(error,response,body) {
 			handle_error(response);
 			var url = body.webservices.findme.url;
-			//console.log("findme url: "+url);
 			iRequest.post({"url":url+"/fmipservice/client/web/initClient",json:{}},
 				function(error,response,body) {
 					handle_error(response);
 					var devices = {}, device, id;
-					// Retrieve each device on the account
-					//console.log(body.content);
-					body.content.forEach(function(device) {
-						//console.log(device.name+"\t"+device.id);
-						if (device.name==device_name) {
-							id = device.id;
-						}
-					});
+					// If no device is passed, take the first one
+					if (!device_name && body.content.length>0) {
+						console.log(body.content[0]);
+						id = body.content[0].id;
+					}
+					else {
+						// Retrieve each device on the account
+						body.content.forEach(function(device) {
+							if (device.name==device_name) {
+								id = device.id;
+							}
+						});
+					}
 					if (id) {
-						//console.log(id);
 						iRequest.post({"url":url+"/fmipservice/client/web/playSound",json:{"subject":"Find My iPhone Alert","device":id}},
 							function(error,response,body) {
 								handle_error(response);
-								//console.log("Notification sent");
+								if (callback) {
+									callback();
+								}
 							}
 						);
 					}
